858d53a0e15ecb4014bc15655b1efce7d8a643e1
Julien Olivain <juju@cotds.org>
2020-09-14 19:56:24 +0200

    examples: square-python: update to Python 3
    This old python example was still using Python 2 syntax. This commit
    update it using the 3.x syntax.
    
    Signed-off-by: Julien Olivain <juju@cotds.org>

--- examples/square_pipe_blocks/square-python.py
+++ examples/square_pipe_blocks/square-python.py
@@ -1,20 +1,23 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 #
 # Execute with:
-#   python square-python.py
+#   python3 square-python.py
 #
 
 import struct
 import sys
 
+DATA_FORMAT = "=f"
+DATA_LEN = struct.calcsize(DATA_FORMAT)
+
 while True:
-    data = sys.stdin.read(4)
+    data = sys.stdin.buffer.read(DATA_LEN)
     if len(data) == 0:
         break
-    elif len(data) == 4:
-        (val,) = struct.unpack("=f", data)
-        #print >> sys.stderr, val
-        outdata = struct.pack("=f", val * val)
-        sys.stdout.write(outdata)
+    elif len(data) == DATA_LEN:
+        (val,) = struct.unpack(DATA_FORMAT, data)
+        # print(val, file=sys.stderr)
+        outdata = struct.pack(DATA_FORMAT, val * val)
+        sys.stdout.buffer.write(outdata)
     else:
-        print >> sys.stderr, "# could not read a float32 (wanted 4 bytes, got", len(data), ")"
+        print("# could not read a float (wanted", DATA_LEN, "bytes, got", len(data), ")", file=sys.stderr)
